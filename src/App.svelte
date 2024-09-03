57e7661fab5dbbd632cd9927139deec2d817cf12
Denzel Braithwaite <denzelbraithwaite@Denzel-Macbook-Work.local>
2024-09-03 13:32:44 -0400

    added some dependencies and cleared some boilerplate code

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,32 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  // Hooks
+  import { onMount } from 'svelte';
+
+  // Modules
+  import Typed from 'typed.js';
+
+  let typed: Typed;
+
+  onMount(() => {
+    typed = new Typed('#page-title', {
+      strings: [
+        '<i>First</i> sentence.',
+        'Second sentence...',
+        'Third...',
+        'Do you get it now?',
+      ],
+      typeSpeed: 50,
+    });
+  })
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="main-content">
+  <h1 id="page-title"></h1>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+<style lang="scss">
+  .main-content {
+    height: 100vh; // dvh not as supported (not at all in firefox)
+    width: 100vw;
   }
 </style>
